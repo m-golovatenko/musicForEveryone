c36b4d2775eb2b048c53ebbfc2160eb450395790
Maria Golovatenko <pikch4y@yandex.com>
2025-09-19 10:44:59 +0300

    feat: add images

--- js/helpData.js
+++ js/helpData.js
@@ -7,9 +7,20 @@ export function renderHelpData() {
     helpData.forEach(item => {
         const clone = template.content.cloneNode(true);
 
-        clone.querySelector(".help__cardTitle").textContent = item.title;
-        clone.querySelector(".help__description").textContent = item.description;
         clone.querySelector(".help__icon").src = item.icon;
+        clone.querySelector(".help__cardTitle").textContent = item.title;
+
+        const descriptionContainer = clone.querySelector(".help__description");
+
+        if (Array.isArray(item.description)) {
+            descriptionContainer.innerHTML =
+                `<ul class="help__list">
+                    ${item.description.map(text => 
+                        `<li class="help__listItem">${text}</li>`).join("")}
+                </ul>`;
+        } else {
+            descriptionContainer.textContent = item.description;
+        }
 
         list.appendChild(clone);
     });
