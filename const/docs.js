59de3e79fa4f0aaa7c1d803262af7e564cb5be48
Maria Golovatenko <pikch4y@yandex.com>
2025-09-22 22:01:33 +0300

    hotfix: fix styles for 2k

--- const/docs.js
+++ const/docs.js
@@ -1,19 +1,19 @@
 export const docs = [
     {
         title: 'Свидетельство',
-        date: '07.03.2025',
+        date: '22.08.2025',
         icon: './images/icons/certificate.svg',
         file: './files/Свидетельство.pdf',
     },
     {
         title: 'Устав',
-        date: '07.03.2025',
+        date: '23.06.2025',
         icon: './images/icons/book.svg',
         file: './files/Устав.pdf',
     },
     {
         title: 'ЕГРЮЛ',
-        date: '07.03.2025',
+        date: '18.07.2025',
         icon: './images/icons/files.svg',
         file: './files/ЕГРЮЛ.pdf',
     }
