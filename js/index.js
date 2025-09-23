4eb9dc10e3cbe697a6aee868a2548ebb207955da
Maria Golovatenko <pikch4y@yandex.com>
2025-09-23 22:04:01 +0300

    hotfix: fix styles for mobile

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {initSpecialistToggle, renderSpecialists} from "./specialists.js";
+import {renderSpecialists} from "./specialists.js";
 import {renderNews} from "./news.js";
 import {renderDocs} from "./docs.js";
 import {renderHelpData} from "./helpData.js";
@@ -7,8 +7,6 @@ import {scrollOnLogo} from "./scroll.js";
 renderHelpData()
 
 renderSpecialists();
-
-initSpecialistToggle();
 
 renderNews()
 
@@ -21,3 +19,14 @@ document.querySelectorAll(".vk").forEach(el => {
         window.open("https://vk.com/club232724284", "_blank");
     });
 });
+
+const burger = document.getElementById('burger')
+const burgerBg = document.getElementById('burgerBg')
+const nav = document.querySelector('.header__nav')
+const contacts = document.querySelector('.header__contacts')
+
+burger.addEventListener('click', () => {
+    burgerBg.classList.toggle('active')
+    nav.classList.toggle('active')
+    contacts.classList.toggle('active')
+})
